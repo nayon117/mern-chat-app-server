f6d116cff77758eccd839265885139207b25e7df
nayon117 <hasibul.nayon1@gmail.com>
2024-02-07 11:28:33 +0600

    create routes and controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req,res) =>{
+    res.send('singup user')
+    console.log('signup')
+}
+
+export const login = (req,res) =>{
+    console.log('login')
+}
+
+export const logout = (req,res) =>{
+    console.log('logout')
+}
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+import express from 'express';
+import dotenv from 'dotenv'
 const app = express()
+
+dotenv.config()
 const port = process.env.PORT ||5000
+import authRoutes from './routes/auth.routes.js'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+ app.use('/api/auth', authRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
