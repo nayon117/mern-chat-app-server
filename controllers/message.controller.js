b83e7f86e0f9c7cafcac44fa7fe327449953e94d
nayon117 <hasibul.nayon1@gmail.com>
2024-02-09 16:41:50 +0600

    sent message successfully in database

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -27,6 +27,12 @@ export const sendMessage = async (req, res) => {
     if(newMessage){
         conversation.messages.push(newMessage._id);
     }
+
+    // await conversation.save();
+    // await newMessage.save();
+    // This will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()])
+
     res.status(201).json(newMessage);
 
    } catch (error) {
