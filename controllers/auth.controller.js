072274ed0f41881b34114fa0bd0a6bda242dc2bc
nayon117 <hasibul.nayon1@gmail.com>
2024-02-09 15:20:57 +0600

    create login function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateToken from "../utils/generateToken.js";
 
+// create user
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -49,8 +50,29 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("login");
+// login user
+export const login = async (req, res) => {
+  try {
+     const { username, password } = req.body;
+     const user = await User.findOne({username})
+     const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+     if(!user || !isPasswordCorrect){
+       return res.status(400).json({message: "Invalid username or password"})
+     }
+
+     generateToken(user._id, res)
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic
+      })
+      
+  } catch (error) {
+    console.log("error in signup", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const logout = (req, res) => {
