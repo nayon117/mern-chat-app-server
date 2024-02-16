77f50789b2041a007bfb794d3f7d407cb8c4b614
nayon117 <hasibul.nayon1@gmail.com>
2024-02-16 09:27:54 +0600

    create user routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config()
 const port = process.env.PORT ||5000
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 
@@ -14,6 +15,7 @@ app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.get('/', (req, res) => {
